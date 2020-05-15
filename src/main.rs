609f7af49613cefd6d0ebc3bea22701e6fc4a110
Jason Dagit <dagitj@gmail.com>
2020-05-15 13:25:26 -0700

    Rewrite to use raw reqwest library

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use twitch_oauth::get_app_access_token;
-
+use serde::{Deserialize, Serialize};
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn read_from_file(fname: &str) -> Result<String> {
+    use std::fs::File;
     use std::io::prelude::*;
     use std::io::BufReader;
-    use std::fs::File;
 
     let f = File::open(fname)?;
     let mut reader = BufReader::new(f);
@@ -14,45 +13,134 @@ fn read_from_file(fname: &str) -> Result<String> {
     Ok(data)
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    use async_tungstenite::tokio::connect_async_with_tls_connector;
-    use tungstenite::{Message};
-    use native_tls::TlsConnector;
-    use futures::stream::StreamExt;
-    use futures::sink::SinkExt;
+fn write_to_file(fname: &str, contents: &str) -> Result<()> {
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    let mut f = File::create(fname)?;
+    f.write_all(contents.as_bytes())?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TokenResponse {
+    access_token: String,
+    refresh_token: String,
+    expires_in: i64,
+    scope: Vec<String>,
+    token_type: String,
+}
+
+fn nonce(length: usize) -> String {
+    use rand::Rng;
+    let mut text = String::new();
+    let possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for _ in 0..length {
+        let idx = rand::thread_rng().gen_range(0, possible.len() - 1) as usize;
+        text += &possible[idx..idx + 1];
+    }
+    text
+}
+
+fn main() -> Result<()> {
+    use std::io::{BufRead, BufReader, Write};
+    use std::net::TcpListener;
+    use url::Url;
 
     let client_secret = read_from_file("client-secret")?;
     let client_id = read_from_file("client-id")?;
-    let token = get_app_access_token(&client_id, &client_secret,
-                                    vec!["chat:read".to_owned()]).await?;
-    println!("{}", token.expires_in);
 
-    let ws_uri = url::Url::parse("wss://pubsub-edge.twitch.tv")?;
-    let cx = TlsConnector::builder().build()?;
-    let cx = tokio_tls::TlsConnector::from(cx);
-    let (mut socket, response) =
-        connect_async_with_tls_connector(ws_uri, Some(cx)).await?;
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
-    }
+    let redirect_url = "http://localhost:8080";
+    let scope = "channel:read:redemptions";
+    let state = nonce(30);
 
-    let listen = format!("{{
-        \"type\": \"LISTEN\",
-        \"nonce\": \"44h1k13746815ab1r2\",
-        \"data\": {{
-            \"topics\": [\"channel-points-channel-v1.61258231\"],
-            \"auth_token\": \"{}\"
-        }}
-    }}", token.access_token);
+    let auth_url = Url::parse(&format!(
+        "https://id.twitch.tv/oauth2/authorize?client_id={}&redirect_uri={}&response_type=code&scope={}&state={}",
+        client_id,
+        redirect_url,
+        scope,
+        state,
+    ))?;
 
-    socket.send(Message::Text(listen.into())).await?;
-    while let Some(message) = socket.next().await {
-        println!("{:#?}", message);
+    println!("Open this URL in your browser:\n{}\n", auth_url.to_string());
+
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Listening on localhost:8080");
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            let code;
+            println!("reading from stream");
+            {
+                let mut reader = BufReader::new(&stream);
+
+                let mut request_line = String::new();
+                reader.read_line(&mut request_line)?;
+                //println!("read: {}", request_line);
+
+                let redirect_url = request_line
+                    .split_whitespace()
+                    .nth(1)
+                    .expect("Unable to split whitespace");
+                let url = Url::parse(&("http://localhost".to_owned() + redirect_url))?;
+
+                let code_pair = url
+                    .query_pairs()
+                    .find(|pair| {
+                        let &(ref key, _) = pair;
+                        key == "code"
+                    })
+                    .expect("No code URL parameter");
+
+                let (_, value) = code_pair;
+                code = value.into_owned();
+                //println!("code = {}", code);
+
+                let state_pair = url
+                    .query_pairs()
+                    .find(|pair| {
+                        let &(ref key, _) = pair;
+                        key == "state"
+                    })
+                    .expect("No state URL parameter");
+
+                let (_, value) = state_pair;
+                let state_uri = value.into_owned();
+                if state == state_uri {
+                    println!("State code matches");
+                } else {
+                    eprintln!("WARNING: State codes DO NOT MATCH! Something went wrong");
+                    return Err("Bad State response".into());
+                }
+                //println!("state = {}", state);
+            }
+
+            let message = "Yay! We got a token from twitch";
+            let response = format!(
+                "HTTP/1.1 200 OK\r\ncontent-length: {}\r\n\r\n{}",
+                message.len(),
+                message
+            );
+            stream.write_all(response.as_bytes())?;
+
+            let reqwest_client = reqwest::blocking::Client::new();
+            let params: &[(&str, &str)] = &[
+                ("client_id", &client_id),
+                ("client_secret", &client_secret),
+                ("code", &code),
+                ("grant_type", "authorization_code"),
+                ("redirect_uri", redirect_url),
+            ];
+            let res: TokenResponse = reqwest_client
+                .post("https://id.twitch.tv/oauth2/token")
+                .form(&params)
+                .send()?
+                .json()?;
+            println!("Twitch responded and parsed");
+            let fname = "oauth-token";
+            write_to_file(fname, &res.access_token)?;
+            println!("oauth token saved to file named: '{}'", fname);
+            break;
+        }
     }
     Ok(())
-
 }
